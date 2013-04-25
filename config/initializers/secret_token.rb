9b97db727c81ba0eea4c6e2a4eb081ec7a31b95a
Noel del Castillo <ngdelcastillo@gmail.com>
2013-04-25 23:08:11 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tourbott::Application.config.secret_key_base = 'c0d27a0640d6cc96133a1b30e13dc807b8607b7a50441d8d0b5095b53c3a5fb25133d235a47262c3db06a2555d3649f8a024e1666f1ce61970413e2785321cd4'
